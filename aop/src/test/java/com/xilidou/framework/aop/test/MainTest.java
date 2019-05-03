5f454eee2b7b4ba4dc10dbdc78629d063a3457e5
xinl <xinlmain@163.com>
2019-05-03 23:50:33 +0800

    增加注释

--- aop/src/test/java/com/xilidou/framework/aop/test/MainTest.java
+++ aop/src/test/java/com/xilidou/framework/aop/test/MainTest.java
@@ -9,6 +9,9 @@ public class MainTest {
         AopApplictionContext aopApplictionContext = new AopApplictionContext("application.json");
         aopApplictionContext.init();
 
+        // 注意，为什么callback能执行到，因为这里获得的bean是testServiceProxy，
+        // 然后将它当作testService来用的，也就是原来的bean被替换了（可以替换的原因是
+        // proxy的父类是TestService）。因为proxy安装了callback，自然就可以实现拦截。
         TestService testService = (TestService) aopApplictionContext.getBean("testServiceProxy");
 
         testService.testMethod();
